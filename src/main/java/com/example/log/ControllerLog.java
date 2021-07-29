4b7930aa71af5ad2f138d635bd2b8aeb69c6d100
sahar-tech <56687581+sahar-tech@users.noreply.github.com>
2021-07-29 08:41:59 +0000

    first1 commit

---
+++ src/main/java/com/example/log/ControllerLog.java
@@ -0,0 +1,27 @@
+package com.example.log;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+//import org.apache.logging.log4j.Logger;
+//import ch.qos.logback.classic.Logger;
+//import java.util.logging.Logger;
+
+@RestController
+public class ControllerLog {
+
+    Logger logger = LoggerFactory.getLogger(ControllerLog.class);
+
+    @RequestMapping("/log")
+    public String index() {
+
+        logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
+
+        return "Howdy! Check out the Logs to see the output...";
+    }
+}
